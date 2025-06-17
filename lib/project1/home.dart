8c3838c73c3bb05bc7a7208da7afaaf271c36c67
Aron Prince Thomas <aronprincethomas27@gmail.com>
2025-06-17 15:31:25 +0530

    added read from firebase

--- lib/project1/home.dart
+++ lib/project1/home.dart
@@ -10,6 +10,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  final CollectionReference donor = FirebaseFirestore.instance.collection('donor');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +27,83 @@ class _HomePageState extends State<HomePage> {
         child: Icon(Icons.add, size: 40,),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+
+      body: StreamBuilder(
+        stream: donor.snapshots(), 
+        builder: (context, AsyncSnapshot snapshot) {
+        if(snapshot.hasData){
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              final DocumentSnapshot donorSnap = snapshot.data.docs[index];
+              return Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  height: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 10,
+                        spreadRadius: 15,
+
+                      )
+                    ]
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: CircleAvatar(
+                            backgroundColor: Colors.red,
+                            radius: 30,
+                            child: Text(donorSnap['group'],style: TextStyle(fontSize: 25),),
+                          ),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            donorSnap['name'],
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            donorSnap['phone'].toString(),
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: (){}, 
+                            icon: Icon(Icons.edit), 
+                            iconSize: 30,
+                            color: Colors.blue,
+                          ),
+                          IconButton(
+                            onPressed: (){}, 
+                            icon: Icon(Icons.delete), 
+                            iconSize: 30,
+                            color: Colors.red,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        }
+        return Container();
+      },
+      ),
     );
   }
 }
