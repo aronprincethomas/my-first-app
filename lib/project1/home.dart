4861ec201b49c27bb98ccd60198f3d01b1d00b53
Aron Prince Thomas <aronprincethomas27@gmail.com>
2025-06-17 16:06:03 +0530

    made the home page list order by name

--- lib/project1/home.dart
+++ lib/project1/home.dart
@@ -29,7 +29,7 @@ class _HomePageState extends State<HomePage> {
       floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
 
       body: StreamBuilder(
-        stream: donor.snapshots(), 
+        stream: donor.orderBy('name').snapshots(),
         builder: (context, AsyncSnapshot snapshot) {
         if(snapshot.hasData){
           return ListView.builder(
